ec826adf1f7539dedf8fdda0fc9deab479c47eab
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2022-01-26 18:24:15 +0530

    Modifications based on Comments Received Part 1

---
+++ cli.py
@@ -0,0 +1,56 @@
+"""CLI for dependency-inspector."""
+import json
+from pathlib import Path
+from typing import Optional
+import typer
+from db.ElasticWorker import connect_elasticsearch
+import configparser
+import logging
+
+from inspector import make_multiple_requests
+
+app = typer.Typer(add_completion=False)
+configfile = configparser.ConfigParser()
+
+
+@app.callback(invoke_without_command=True)
+def main(
+        config: Optional[Path] = typer.Option(None),
+        host: str = typer.Option("localhost"),
+        port: int = typer.Option(9200), es_db: bool = False,
+        gh_token: Optional[str] = typer.Option(None),
+        es_uid: Optional[str] = typer.Option(None),
+        es_pass: Optional[str] = typer.Option(None)
+):
+    """Main function for CLI"""
+    if config is None or not config.is_file():
+        logging.error("Configuration file not found")
+        raise typer.Abort()
+    configfile.read(config)
+    if gh_token is None and configfile.has_option("secrets", "gh_token"):
+        gh_token = configfile.get("secrets", "gh_token").strip()
+    if es_uid is None and configfile.has_option("secrets", "es_uid"):
+        es_uid = configfile.get("secrets", "es_uid").strip()
+    if es_pass is None and configfile.has_option("secrets", "es_pass"):
+        es_pass = configfile.get("secrets", "es_pass").strip()
+    if es_db:
+        es = connect_elasticsearch({'host': host, 'port': port}, (es_uid, es_pass))
+    else:
+        es = None
+    if not configfile.has_section("dependencies"):
+        logging.error("dependencies section missing from config file")
+        raise typer.Abort()
+    for lang, dependencies in configfile["dependencies"].items():
+        logging.info(
+            json.dumps(
+                make_multiple_requests(
+                    es, lang, dependencies.split("\n"), gh_token
+                ),
+                indent=3
+            )
+        )
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+    app()
--- helper.py
+++ helper.py
@@ -1,19 +1,10 @@
-"""Helper Functions for Inspector"""
-
+"""Helper Functions for Inspector."""
+import logging
+import re
 from typing import TypedDict, List
-
-
-def parse_license(license_file: str, license_dict: dict) -> str:
-    """
-    Checks the license file contents to return possible license type
-    :param license_file: String containing license file contents
-    :param license_dict: Dictionary mapping license files and unique substring
-    :return: Detected license type as a String, Other if failed to detect
-    """
-    for lic in license_dict:
-        if lic in license_file:
-            return license_dict[lic]
-    return "Other"
+import requests
+import jmespath
+from bs4 import BeautifulSoup
 
 
 class Result(TypedDict):
@@ -24,3 +15,113 @@ class Result(TypedDict):
     license: str
     dependencies: List[str]
     timestamp: str
+
+
+def parse_license(license_file: str, license_dict: dict) -> str:
+    """
+    Check license file content and return the possible license type.
+    :param license_file: String containing license file content
+    :param license_dict: Dictionary mapping license files and unique substring
+    :return: Detected license type as a String, `Other` if failed to detect
+    """
+    for lic in license_dict:
+        if lic in license_file:
+            return license_dict[lic]
+    return "Other"
+
+
+def handle_pypi(api_response: requests.Response, queries: dict, result: Result):
+    """
+    Take api response and return required results object
+    :param api_response: response from requests get
+    :param queries: compiled jmespath queries
+    :param result: object to mutate
+    """
+    version_q: jmespath.parser.ParsedResult = queries['version']
+    license_q: jmespath.parser.ParsedResult = queries['license']
+    dependencies_q: jmespath.parser.ParsedResult = queries['dependency']
+    data = api_response.json()
+    result['version'] = version_q.search(data)
+    result['license'] = license_q.search(data)
+    result['dependencies'] = dependencies_q.search(data)
+    return result
+
+
+def handle_npmjs(api_response: requests.Response, queries: dict, result: Result):
+    """
+    Take api response and return required results object
+    :param api_response: response from requests get
+    :param queries: compiled jmespath queries
+    :param result: object to mutate
+    """
+    data = api_response.json()
+    version_q: jmespath.parser.ParsedResult = queries['version']
+    license_q: jmespath.parser.ParsedResult = queries['license']
+    dependencies_q: jmespath.parser.ParsedResult = queries['dependency']
+    version = version_q.search(data)
+    if version is not None:
+        result['version'] = version
+    else:
+        latest_q: jmespath.parser.ParsedResult = queries['latest']
+        latest = latest_q.search(data)
+        result['version'] = latest
+        data = jmespath.search(
+            queries['versions'].format(latest),
+            data
+        )
+    result['license'] = ";".join(license_q.search(data))
+    result['dependencies'] = dependencies_q.search(data)
+
+
+def scrape_go(response: requests.Response, queries: dict, result: Result, url: str):
+    """
+    Take api response and return required results object
+    :param response: response from requests get
+    :param queries: compiled jmespath queries
+    :param result: object to mutate
+    :param url: go url scraped
+    """
+    soup = BeautifulSoup(response.text, "html.parser")
+    name_parse = queries['name'].split('.')
+    name_data = soup.find(
+        name_parse[0],
+        class_=name_parse[1]
+    ).getText().strip().split(" ")
+    package_name = result['name']
+    if len(name_data) > 1:
+        package_name = name_data[-1].strip()
+    key_parse = queries['parse'].split('.')
+    ver_parse = queries['versions'].split('.')
+    dep_parse = queries['dependencies'].split('.')
+    key_element = soup.find(
+        key_parse[0],
+        class_=key_parse[1]
+    ).getText()
+    key_data = re.findall(r"([^ \n:]+): ([a-zA-Z0-9-_ ,.]+)", key_element)
+    data = dict(key_data)
+    ver_res = requests.get(url + "?tab=versions", allow_redirects=False)
+    dep_res = requests.get(url + "?tab=imports", allow_redirects=False)
+    if ver_res.status_code == 200:
+        version_soup = BeautifulSoup(ver_res.text, "html.parser")
+        releases = [
+            release.getText().strip()
+            for release in version_soup.findAll(
+                ver_parse[0],
+                class_=ver_parse[1]
+            )
+        ]
+        logging.info(releases)
+    dependencies_tag = []
+    if dep_res.status_code == 200:
+        dep_soup = BeautifulSoup(dep_res.text, "html.parser")
+        dependencies_tag = [
+            dependency.getText().strip()
+            for dependency in dep_soup.findAll(
+                dep_parse[0],
+                class_=dep_parse[1]
+            )
+        ]
+    result['name'] = package_name
+    result['version'] = data[queries['version']]
+    result['license'] = data[queries['license']]
+    result['dependencies'] = dependencies_tag
