a687c84dd7567ba1babdf7eddb53ca6cca55e634
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2022-02-24 13:12:44 +0530

    feat: cleans up output into required format

--- dependencies/js/js_worker.py
+++ dependencies/js/js_worker.py
@@ -11,7 +11,13 @@ def handle_yarn_lock(req_file_data: str) -> dict:
     :param req_file_data: Content of yarn.lock
     :return: list of requirement and specs
     """
-    res = {}
+    res = {
+        "lang_ver": "",
+        "pkg_name": "",
+        "pkg_ver": "",
+        "pkg_lic": "",
+        "pkg_dep": [],
+    }
     if "lockfile v1" in req_file_data:
         parsed_lockfile = lockfile.Lockfile.from_str(req_file_data)
         unfiltered_content: dict = json.loads(
@@ -19,21 +25,30 @@ def handle_yarn_lock(req_file_data: str) -> dict:
         )
     else:
         unfiltered_content = yaml.safe_load(req_file_data)
-    keys = ["resolution", "dependencies"]
-    for name, obj in unfiltered_content.items():
-        flat = {}
-        gen = (x for x in keys if x in obj)
-        for k in gen:
-            if isinstance(obj[k], dict):
-                flat[k] = list(
-                    map(
-                        lambda x: str(x[0]) + ";" + str(x[1]),
-                        obj[k].items()
-                    )
-                )
-            else:
-                flat[k] = obj[k]
-        res[name] = flat
+    for package in unfiltered_content.keys():
+        if package.startswith("_"):
+            continue
+        res["pkg_dep"].append(
+            str(package.split(",")[0].rsplit("@", 1)[0]) + ";" +
+            str(unfiltered_content[package].get("version", ""))
+        )
+    # handle sub_dep
+    #
+    # keys = ["resolution", "dependencies"]
+    # for name, obj in unfiltered_content.items():
+    #     flat = {}
+    #     gen = (x for x in keys if x in obj)
+    #     for k in gen:
+    #         if isinstance(obj[k], dict):
+    #             flat[k] = list(
+    #                 map(
+    #                     lambda x: str(x[0]) + ";" + str(x[1]),
+    #                     obj[k].items()
+    #                 )
+    #             )
+    #         else:
+    #             flat[k] = obj[k]
+    #     res[name] = flat
     return res
 
 
@@ -55,21 +70,53 @@ def handle_json(req_file_data: str) -> dict:
     }
     for k, v in filter_dict.items():
         if k is "pkg_dep":
-            filter_dict[k] = [";".join(i) for i in v.items()]
-        elif isinstance(v, dict):
-            filter_dict[k] = ";".join(v.values())
-        elif isinstance(v, list):
-            if any(
-                    isinstance(i, dict)
-                    for i in v
-            ):
-                temp_list = [
-                    ";".join(s.values())
-                    for s in v
-                    if isinstance(s, dict)
-                ]
-                filter_dict[k] = ";".join(temp_list)
-            else:
-                filter_dict[k] = ";".join(v)
+            handle_json_dep(filter_dict, k, v)
+        else:
+            flatten_content(filter_dict, k, v)
     filter_dict["pkg_err"] = ""
     return filter_dict
+
+
+def handle_json_dep(filter_dict, k, v):
+    """
+    Flattens variants of dependencies to uniform
+    :param filter_dict: any dict or list
+    :param k: key to check
+    :param v: associated value
+    """
+    if any(
+            isinstance(i, dict)
+            for i in v.values()
+    ):
+        filter_dict[k] = [
+            i + ";" + v[i].get("version", "")
+            for i in v.keys()
+        ]
+    else:
+        filter_dict[k] = [";".join(i) for i in v.items()]
+
+
+def flatten_content(filter_dict, k, v):
+    """
+    Flattens a dict/list - used to handle deprecated formats
+    :param filter_dict: any dict or list
+    :param k: key to check
+    :param v: associated value
+    """
+    if isinstance(v, dict):
+        filter_dict[k] = ";".join(v.values())
+    elif isinstance(v, list):
+        if any(
+                isinstance(i, dict)
+                for i in v
+        ):
+            temp_list = [
+                ";".join(s.values())
+                for s in v
+                if isinstance(s, dict)
+            ]
+            filter_dict[k] = ";".join(temp_list)
+        else:
+            filter_dict[k] = ";".join(v)
+    else:
+        filter_dict[k] = str(filter_dict[k])
