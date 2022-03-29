f3a6929f0e05fdcf7f340ec64d8d2457667a15df
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2022-03-29 19:34:38 +0530

    dev: sample postgres worker

--- handle_env.py
+++ handle_env.py
@@ -5,6 +5,7 @@ import os
 
 from dotenv import load_dotenv
 from github import Github
+import psycopg2.extras
 
 load_dotenv()
 
@@ -16,8 +17,35 @@ else:
 github_object = Github(gh_token)
 
 
-def get_github() -> Github:
+conn = None
+if "HOSTNAME" in os.environ:
+    HOSTNAME    = os.environ.get("HOSTNAME")
+    DATABASE    = os.environ.get("DATABASE")
+    USERNAME    = os.environ.get("USERNAME")
+    PWD         = os.environ.get("PWD")
+    PORT_ID     = os.environ.get("PORT_ID")
+    try:
+        conn = psycopg2.connect(
+            host = HOSTNAME,
+            dbname = DATABASE,
+            user = USERNAME,
+            password = PWD,
+            port = PORT_ID
+        )
+    except Exception as error:
+        logging.error(error)
+
+
+def get_github():
     """
     Returns an authenticated GitHub object if env variable is defined
     """
     return github_object
+
+
+
+def get_db():
+    """
+    Returns an authenticated Postgres connection
+    """
+    return conn
