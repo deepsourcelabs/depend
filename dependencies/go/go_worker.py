56cabd592244b4905343db112339afcd75c05f18
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2022-04-19 09:30:51 +0530

    Parsing go.mod (#12)
    * feat: Implements go.mod parser and library source files
    
    * feat: adds interface with so files for parsing go.mod
    
    * test: adds test for extracting info from go.mod
    
    * fix: fixes issues with so files across platforms

---
+++ dependencies/go/go_worker.py
@@ -0,0 +1,34 @@
+"""Functions to handle Go files"""
+import json
+import logging
+from ctypes import cdll, c_char_p, c_void_p, string_at
+import platform
+
+match platform.system():
+    case "Linux":
+        lib_go = cdll.LoadLibrary("dependencies/go/linux/_gomod.so")
+    case "Windows":
+        lib_go = cdll.LoadLibrary("dependencies/go/win32/_gomod.so")
+    case _:
+        lib_go = None
+        logging.error("Not compiled for Darwin")
+
+getDepVer = lib_go.getDepVer
+getDepVer.argtypes = [c_char_p]
+getDepVer.restype = c_void_p
+free = lib_go.freeCByte
+free.argtypes = [c_void_p]
+
+
+def handle_go_mod(req_file_data: str) -> dict:
+    """
+    Parse go.mod file
+    :param req_file_data: Content of go.mod
+    :return: list of requirement and specs
+    """
+    ptr = getDepVer(
+        req_file_data.encode('utf-8')
+    )
+    out = string_at(ptr).decode('utf-8')
+    free(ptr)
+    return json.loads(out)
